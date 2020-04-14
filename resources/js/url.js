ca24f54e00522162ef4dc3b8002e7381a9ae5103
websturing <websturing.project@gmail.com>
2020-04-14 10:37:27 +0700

    rubah url

--- resources/js/url.js
+++ resources/js/url.js
@@ -2,9 +2,14 @@
 // const urlBase = "inilahkepri_mobile";
 // const urlWeb = "http://localhost/inilahkepri_mobile";
 
-const urlAxios = "http://192.168.100.8/inilahkepri_mobile/api";
+// const urlAxios = "http://192.168.100.8/inilahkepri_mobile/api";
+// const urlBase = "inilahkepri_mobile";
+// const urlWeb = "http://192.168.100.8/inilahkepri_mobile";
+
+const urlAxios = "http://tukangketik.my.id/inilahkepri_mobile/api";
 const urlBase = "inilahkepri_mobile";
-const urlWeb = "http://192.168.100.8/inilahkepri_mobile";
+const urlWeb = "http://tukangketik.my.id/inilahkepri_mobile";
+
 
 export default {
   urlAxios: urlAxios,
