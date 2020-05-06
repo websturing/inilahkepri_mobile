3c81754df3425c4a6c2faec10372dc8bebe6f88a
websturing <websturing.project@gmail.com>
2020-05-06 20:39:49 +0700

    change local to online

--- resources/js/url.js
+++ resources/js/url.js
@@ -1,16 +1,16 @@
-// const urlAxios = "http://localhost/inilahkepri_mobile/api";
-// const urlBase = "inilahkepri_mobile";
-// const urlWeb = "http://localhost/inilahkepri_mobile";
-// const urlGambar = "http://localhost/inilahkepri_mobile";
+const urlAxios = "http://localhost/inilahkepri_mobile/api";
+const urlBase = "inilahkepri_mobile";
+const urlWeb = "http://localhost/inilahkepri_mobile";
+const urlGambar = "http://localhost/inilahkepri_mobile";
 
 // const urlAxios = "http://192.168.100.8/inilahkepri_mobile/api";
 // const urlBase = "inilahkepri_mobile";
 // const urlWeb = "http://192.168.100.8/inilahkepri_mobile";
 
-const urlAxios = "http://m.inilahkepri.id/api";
-const urlBase = "";
-const urlWeb = "http://m.inilahkepri.id";
-const urlGambar = "http://inilahkepri.id";
+// const urlAxios = "http://m.inilahkepri.id/api";
+// const urlBase = "";
+// const urlWeb = "http://m.inilahkepri.id";
+// const urlGambar = "http://inilahkepri.id";
 
 export default {
   urlAxios: urlAxios,
