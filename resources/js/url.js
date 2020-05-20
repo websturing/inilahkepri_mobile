bbbe206b4c9444e1469cacc07e361a473b50f7ba
websturing <websturing.project@gmail.com>
2020-05-20 11:56:22 +0700

    add urlGambar to url

--- resources/js/url.js
+++ resources/js/url.js
@@ -6,6 +6,7 @@
 const urlAxios = "http://tukangketik.my.id/inilahkepri_mobile/api";
 const urlBase = "inilahkepri_mobile";
 const urlWeb = "http://tukangketik.my.id/inilahkepri_mobile";
+const urlGambar = "http://inilahkepri.id";
 
 //const urlAxios = "http://m.inilahkepri.id/api";
 //const urlBase = "";
