9a28e88e39d3b23edf54adb2152f993007e2b8dc
websturing <websturing.project@gmail.com>
2020-05-20 11:53:01 +0700

    change local to virtual online

--- resources/js/components/berita/detail.vue
+++ resources/js/components/berita/detail.vue
@@ -1,129 +1,130 @@
 <template>
-<div class="site-section" style="overflow: hidden;">
-    <div class="container detailImage">
-     <br/><br/><br/>
+    <div class="site-section" style="overflow: hidden;">
+        <div class="container detailImage">
+            <br /><br /><br />
             <h5>{{detail.judul}}</h5>
 
             <div class="share-sosial">
-              <el-row :gutter="5">
+                <el-row :gutter="5">
                     <el-col :span="24">
                         <div class="is-kategori">{{moment(tglPublish).format('LLLL')}}</div>
                     </el-col>
                     <el-col :span="24">
                         <div class="addthis_inline_share_toolbox"></div>
                     </el-col>
-              </el-row>
+                </el-row>
             </div>
 
             <img :src="url.image+'/'+detail.Folder+'/'+detail.gambar" alt="Placeholder image" class="img-fluid">
             <p class="ketGambar">{{detail.ket_gambar}}</p>
-            <el-divider></el-divider>
-            {{hIsiBerita}}
             <p v-html="detail.isi_berita" ref="isiBerita" style="position:relative;"></p>
-             <h1 :style="{'margin-top': hIsiBerita}" class="bacaJuga">Afriandi</h1>
-             <div style="margin-bottom : 761.5px"></div>
-          
+
+        </div>
+        <berita :take="5" :skip="0" :type="'berita'" :infinite="true" />
     </div>
-      <berita :take="5" :skip="0" :type="'berita'" :infinite="true"/>
-</div>
 </template>
 
 
 <script>
-import berita from '@/js/components/berita/berita';
-import urlBase from '@/js/url';
-import moment from 'moment';
-import Parallax from 'vue-parallaxy'
+    import berita from '@/js/components/berita/berita';
+    import urlBase from '@/js/url';
+    import moment from 'moment';
+    import Parallax from 'vue-parallaxy'
 
-export default {
-    data(){
-        return{
-            idNew : 0,
-            // id : 4202,
-            berita:{
-                data:[]
-            },
-            detail :{},
-            url : {
-                api : urlBase.urlAxios,
-                image : urlBase.urlGambar+'/resources/Artikel_Thumbnail/',
-            },
-            hIsiBerita : null
-        }
-    },
-    props:['id'],
-    computed:{
-        tglPublish: function () {
-            return this.detail.tgl_publish+" "+this.detail.jam
+    export default {
+        data() {
+            return {
+                idNew: 0,
+                // id : 4202,
+                berita: {
+                    data: []
+                },
+                detail: {},
+                url: {
+                    api: urlBase.urlAxios,
+                    image: urlBase.urlGambar + '/resources/Artikel_Thumbnail/',
+                },
+                hIsiBerita: null
+            }
         },
-        heifh: function(){
-           return this.heightOfIsi()
-        }
-    },
-    created(){
-        this.getBerita();       
-    },
-    mounted(){
-        this.$parent.$data.activeLoader = false;
-    },
-    methods:{
-        moment(arg) {
-             moment.locale('id');
-             return moment(arg);
+        props: ['id'],
+        computed: {
+            tglPublish: function() {
+                return this.detail.tgl_publish + " " + this.detail.jam
+            },
+            heifh: function() {
+                return this.heightOfIsi()
+            }
         },
-        getBerita(){
-            axios.post(this.url.api+'/berita',{
-                type : "id",
-                id : this.id
-            })
-                .then(r => {
-                    this.detail = r.data[0],
-                    this.heightOfIsi()
+        created() {
+            this.getBerita();
+        },
+        mounted() {
+            this.$parent.$data.activeLoader = false;
+        },
+        methods: {
+            moment(arg) {
+                moment.locale('id');
+                return moment(arg);
+            },
+            getBerita() {
+                axios.post(this.url.api + '/berita', {
+                        type: "id",
+                        id: this.id
+                    })
+                    .then(r => {
+                        this.detail = r.data[0],
+                            this.heightOfIsi()
+                    });
+            },
+            heightOfIsi() {
+                this.$nextTick(() => {
+
+                    let h = "-" + this.$refs.isiBerita.clientHeight * 0.5 + 'px'
+                    this.hIsiBerita = h
+
                 });
+
+            }
         },
-        heightOfIsi(){
-            this.$nextTick(() => {
-
-                 let h = "-"+this.$refs.isiBerita.clientHeight * 0.5+'px'
-                 this.hIsiBerita = h
-
-            });
-           
+        components: {
+            berita,
+            Parallax
         }
-    },
-    components:{
-        berita,
-        Parallax
     }
-}
+
 </script>
 <style scoped>
-h5{
-    font-weight: 900;
-    color: #333;
-}
-p{
-    color: #333;
-}
-.ketGambar{
-    font-size: 11px !important;
-    font-style: italic;
-    
-}
+    h5 {
+        font-weight: 900;
+        color: #333;
+    }
 
-.is-kategori{
-    font-size: 12px !important;
-}
+    p {
+        color: #333;
+    }
 
-.detailImage img{
-    border-radius: 5px;
-}
-.share-sosial{
-    margin: 10px 0px 15px 0px;
-}
+    .ketGambar {
+        font-size: 11px !important;
+        font-style: italic;
 
-.bacaJuga{
-    display: absolute;
-    background: red;
-}
+    }
+
+    .is-kategori {
+        font-size: 12px !important;
+    }
+
+    .detailImage img {
+        border-radius: 5px;
+    }
+
+    .share-sosial {
+        margin: 10px 0px 15px 0px;
+    }
+
+    .bacaJuga {
+        display: absolute;
+        background: red;
+    }
+
 </style>
