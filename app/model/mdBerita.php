4b0259ac705441a851a0270c56bb2a2d208c211a
websturing <websturing.project@gmail.com>
2020-05-28 21:34:29 +0700

    viewer detail

--- app/model/mdBerita.php
+++ app/model/mdBerita.php
@@ -11,7 +11,7 @@ class mdBerita extends Model
     protected $table        = "berita";
     protected $primaryKey   = "id_berita";
 
-    public $timestamp = false;
+   public $timestamps = false;
     
     protected $appends = [
         'Folder',
