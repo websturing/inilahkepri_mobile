819910e411f8b77dde4f95c44705f253ecdf6789
websturing <websturing.project@gmail.com>
2020-05-28 17:58:38 +0700

    local to online

--- resources/js/url.js
+++ resources/js/url.js
@@ -2,20 +2,27 @@
 // const urlBase = "inilahkepri_mobile";
 // const urlWeb = "http://localhost/inilahkepri_mobile";
 // const urlGambar = "http://inilahkepri.id";
-
-const urlAxios = "http://tukangketik.my.id/inilahkepri_mobile/api";
-const urlBase = "inilahkepri_mobile";
-const urlWeb = "http://tukangketik.my.id/inilahkepri_mobile";
-const urlGambar = "http://inilahkepri.id";
-
-//const urlAxios = "http://m.inilahkepri.id/api";
-//const urlBase = "";
-//const urlWeb = "http://m.inilahkepri.id";
+//
+//const urlAxios = "http://tukangketik.my.id/inilahkepri_mobile/api";
+//const urlBase = "inilahkepri_mobile";
+//const urlWeb = "http://tukangketik.my.id/inilahkepri_mobile";
 //const urlGambar = "http://inilahkepri.id";
+
+const urlAxios = "http://m.inilahkepri.id/api";
+const urlBase = "";
+const urlWeb = "http://m.inilahkepri.id";
+const urlGambar = "http://inilahkepri.id";
+const urlThumbnailBerita = "http://inilahkepri.id/resources/Artikel_Thumbnail";
+const iklan = "http://inilahkepri.id/public/iklan";
+const album = "http://inilahkepri.id/resources/album";
+const gallery = "http://inilahkepri.id/resources/gallery";
 
 export default {
     urlAxios: urlAxios,
     urlBase: urlBase,
     urlWeb: urlWeb,
     urlGambar: urlGambar,
+    iklan: iklan,
+    album: album,
+    gallery: gallery,
 }
