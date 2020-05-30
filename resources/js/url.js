6468755042c911eafdad78d4868a37df31b064c3
websturing <websturing.project@gmail.com>
2020-05-30 20:47:40 +0700

    redirect beranda

--- resources/js/url.js
+++ resources/js/url.js
@@ -6,27 +6,27 @@
 // const album = "https://inilahkepri.id/resources/album";
 // const gallery = "https://inilahkepri.id/resources/gallery";
 
- //
- //const urlAxios = "http://tukangketik.my.id/inilahkepri_mobile/api";
- //const urlBase = "inilahkepri_mobile";
- //const urlWeb = "http://tukangketik.my.id/inilahkepri_mobile";
- //const urlGambar = "http://inilahkepri.id";
+//
+//const urlAxios = "http://tukangketik.my.id/inilahkepri_mobile/api";
+//const urlBase = "inilahkepri_mobile";
+//const urlWeb = "http://tukangketik.my.id/inilahkepri_mobile";
+//const urlGambar = "http://inilahkepri.id";
 
- const urlAxios = "https://m.inilahkepri.id/api";
- const urlBase = "";
- const urlWeb = "https://m.inilahkepri.id";
- const urlGambar = "https://inilahkepri.id";
- const urlThumbnailBerita = "https://inilahkepri.id/resources/Artikel_Thumbnail";
- const iklan = "https://inilahkepri.id/public/iklan";
- const album = "https://inilahkepri.id/resources/album";
- const gallery = "https://inilahkepri.id/resources/gallery";
+const urlAxios = "https://m.inilahkepri.id/api";
+const urlBase = "";
+const urlWeb = "https://m.inilahkepri.id";
+const urlGambar = "https://inilahkepri.id";
+const urlThumbnailBerita = "https://inilahkepri.id/resources/Artikel_Thumbnail";
+const iklan = "http://inilahkepri.id/public/iklan";
+const album = "http://inilahkepri.id/resources/album";
+const gallery = "http://inilahkepri.id/resources/gallery";
 
- export default {
-     urlAxios: urlAxios,
-     urlBase: urlBase,
-     urlWeb: urlWeb,
-     urlGambar: urlGambar,
-     iklan: iklan,
-     album: album,
-     gallery: gallery,
- }
+export default {
+    urlAxios: urlAxios,
+    urlBase: urlBase,
+    urlWeb: urlWeb,
+    urlGambar: urlGambar,
+    iklan: iklan,
+    album: album,
+    gallery: gallery,
+}
